e95c57758021897592fdb1d246da2e65242898c7
sMythicalBird <1778133880@qq.com>
2024-07-13 15:31:52 +0800

    update

--- utils/config.py
+++ utils/config.py
@@ -17,8 +17,9 @@ if not config_path.exists():
     logger.error(f"config.yaml 未在 {config_path} 发现")
     with open(config_path, "w", encoding="utf-8") as f:
         safe_dump(Config().model_dump(), f)
-    logger.error(f"请参照 config.example.yaml 修改配置")
-    exit(1)
+    # logger.error(f"请参照 config.example.yaml 修改配置")
+    # exit(1)
+    logger.info(f"已生成默认配置文件 config.yaml")
 with open(config_path, "r", encoding="utf-8") as f:
     config = safe_load(f)
 config = Config(**config)
