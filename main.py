5b99dc223e333789129802311f76fe3b2b5d298b
SuperLazyDog <yun981128@gmail.com>
2024-07-19 12:33:49 +0800

    [update] 将dll_path临时添加到环境变量

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
+import os
+import sys
+from pathlib import Path
+
+dll_path = Path(sys.executable).parent / "Library" / "bin"
+current_path = os.environ.get("PATH", "")
+if dll_path.exists() and str(dll_path) not in current_path:
+    # 将新的目录添加到 PATH 的开始或结束
+    # 这里我们添加到末尾，并用分号或冒号（取决于操作系统）分隔
+    current_path = str(dll_path) + os.pathsep + current_path
+
+nvidia_path = Path(sys.executable).parent / "Lib" / "site-packages" / "nvidia"
+if nvidia_path.exists():
+    for bin_path in nvidia_path.iterdir():
+        bin_path = bin_path / "bin"
+        if bin_path.is_dir() and str(bin_path) not in current_path:
+            current_path = str(bin_path) + os.pathsep + current_path
+os.environ["PATH"] = current_path
 print(
     "\n --------------------------------------------------------------"
     "\n     注意：此脚本为免费的开源软件，如果你是通过购买获得的，那么你受骗了！\n "
     "--------------------------------------------------------------\n"
 )
-print(
-    "使用说明：\n   F10  恢复运行\n   F11  暂停运行\n   F12  结束运行\n"
-)
+print("使用说明：\n   F10  恢复运行\n   F11  暂停运行\n   F12  结束运行\n")
 from threading import Thread
 from pynput.keyboard import Key, Listener
 from utils.task import task
