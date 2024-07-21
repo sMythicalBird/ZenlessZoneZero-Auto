0967b7bd9affbe82c8b6b09013abe98fcdb9f647
sMythicalBird <1778133880@qq.com>
2024-07-21 15:33:55 +0800

    新增DLC:拿命验收update

--- dlc/money.py
+++ dlc/money.py
@@ -7,8 +7,9 @@
 import time
 from typing import Dict
 from schema import Position
-from utils import control
+from utils import control, screenshot
 from utils.task import task
+from re import template
 from pydirectinput import (
     press,
     click,
@@ -24,6 +25,24 @@ from pydirectinput import (
 fflag = 0
 
 
+# def get_pos(text: str):
+#     text = template(text)
+#     img = screenshot()  # 截图
+#     ocr_Results = task.ocr(img)  # OCR识别
+#     # print(ocr_Results)
+#     positions = []
+#     for ocr_result in ocr_Results:
+#         if text.search(ocr_result.text):
+#             # print(ocr_result)
+#             positions.append(
+#                 [
+#                     (ocr_result.position[0] + ocr_result.position[2]) / 2,
+#                     (ocr_result.position[1] + ocr_result.position[3]) / 2,
+#                 ]
+#             )
+#     return positions
+
+
 def money_fight():
     moveRel(110, 0, relative=True)
     time.sleep(0.3)
@@ -37,34 +56,39 @@ def money_fight():
     press("space", duration=0.1)
 
 
-def money_go(t1: float, t2: float, t3: float):
+def money_go(t1: float, t2: float, t3: int):
     time.sleep(t1)
     keyDown("d")
     time.sleep(t2)
     keyUp("d")
-    time.sleep(t3)
+    for i in range(t3):
+        time.sleep(0.1)
+        press("f", duration=0.1)
     keyUp("shift")
     keyUp("w")
 
 
 # 战斗
 @task.page(name="战斗中", target_texts=["^Space$"])
-def action(positions: Dict[str, Position]):
+def action():
     global fflag
     # 击碎箱子
-    time.sleep(0.2)
     money_fight()
     # 交付任务
-    money_go(3.5, 0.5, 0.5)
-    time.sleep(0.3)
-    press("f", duration=0.1)
+    money_go(3.2, 0.5, 5)
     fflag = 1
+    for i in range(5):
+        control.click(1020, 440)
+        time.sleep(0.2)
+    press("esc", duration=0.1)
 
 
 # 选择委托
 @task.page(name="选择委托", target_texts=["^战斗委托$"])
 def action(positions: Dict[str, Position]):
     pos = positions.get("^战斗委托$")
+    control.click(pos.x, pos.y)
+    time.sleep(0.1)
     control.click(pos.x, pos.y)
 
 
@@ -82,32 +106,10 @@ def action(positions: Dict[str, Position]):
 @task.page(name="出战", target_texts=["^出战$"])
 def action(positions: Dict[str, Position]):
     global fflag
-    time.sleep(0.3)
+    # time.sleep(0.3)
     pos = positions.get("^出战$")
     control.click(pos.x, pos.y)
     fflag = 0
-
-
-# 结尾对话
-@task.page(name="对话1", target_texts=["不堪一击"], exclude_texts=["确实如此"])
-def action():
-    press("space", duration=0.1)
-    time.sleep(0.2)
-
-
-@task.page(name="对话2", target_texts=["确实如此", "太脆弱了"])
-def action(positions: Dict[str, Position]):
-    pos = positions.get("确实如此")
-    control.click(pos.x, pos.y)
-    time.sleep(0.5)
-    press("space", duration=0.1)
-
-
-# 结尾对话
-@task.page(name="对话3", target_texts=["把录像发给我"])
-def action():
-    press("space", duration=0.1)
-    time.sleep(0.2)
 
 
 # 通关
@@ -115,7 +117,23 @@ def action():
 def action(positions: Dict[str, Position]):
     pos = positions.get("^完成$")
     control.click(pos.x, pos.y)
-    time.sleep(0.3)
+    time.sleep(0.1)
+    control.click(pos.x, pos.y)
+
+
+# 重新开始
+@task.page(name="重新开始", target_texts=["^重新开始$"])
+def action(positions: Dict[str, Position]):
+    pos = positions.get("^重新开始$")
+    control.click(pos.x, pos.y)
+    global fflag
+    fflag = 0
+
+
+@task.page(name="确认", target_texts=["^确认"])
+def action(positions: Dict[str, Position]):
+    pos = positions.get("^确认")
+    control.click(pos.x, pos.y)
 
 
 @task.page(name="休息_离开", target_texts=["^离开$"])
@@ -126,18 +144,18 @@ def action(positions: Dict[str, Position]):
 
 @task.page(name="主界面", target_texts=["^影像档案架$"])
 def action():
-    time.sleep(1)
+    time.sleep(0.5)
     moveRel(400, 0, relative=True)
-    time.sleep(1)
+    time.sleep(0.5)
     press("w", duration=0.2)
-    time.sleep(1)
+    time.sleep(0.5)
     moveRel(380, 0, relative=True)
-    time.sleep(1)
+    time.sleep(0.5)
     press("w", duration=0.8)
-    time.sleep(1)
+    time.sleep(0.5)
     moveRel(-400, 0, relative=True)
-    time.sleep(1)
-    press("w", duration=0.3)
+    time.sleep(0.5)
+    press("w", duration=0.6)
     time.sleep(0.3)
     press("f", duration=0.1)
 
