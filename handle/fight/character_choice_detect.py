51335cc1826b55e59a47d86ac4deb7d5617b4233
flashzs <flashzs@qq.com>
2024-07-28 14:18:58 +0800

    [update] 优化水平进度条边缘检测方法

--- handle/fight/character_choice_detect.py
+++ handle/fight/character_choice_detect.py
@@ -5,17 +5,37 @@ import numpy as np
 import os
 
 
-def canny_edge_detect_BGR(img: np.ndarray) -> np.ndarray:
+def canny_edge_detect(img: np.ndarray) -> np.ndarray:
     """
     针对BGR三通道的边缘检测
     :param img: 输入图像 RGB 格式
     """
     b, g, r = cv2.split(img)
-    edge_b = cv2.Canny(b, 100, 200)
-    edge_g = cv2.Canny(g, 100, 200)
-    edge_r = cv2.Canny(r, 100, 200)
+    threshold = (50, 150)
+    edge_b = cv2.Canny(b, threshold[0], threshold[1])
+    edge_g = cv2.Canny(g, threshold[0], threshold[1])
+    edge_r = cv2.Canny(r, threshold[0], threshold[1])
     edge_BGR = cv2.merge([edge_b, edge_g, edge_r])
+
     edge_BGR2GRAY = cv2.cvtColor(edge_BGR, cv2.COLOR_BGR2GRAY)
+
+    def h_bar_edge_detect(img, edge):
+        """
+        水平进度条边缘检测优化
+        水平进度条区域使用灰度图像，高斯模糊后进行边缘检测，再与原图边缘对应区域进行合并
+        """
+        sub_x1 = 85
+        sub_y1 = 20
+        sub_x2 = 565
+        sub_y2 = 68
+        sub_img = img[sub_y1:sub_y2, sub_x1:sub_x2]
+        sub_img_gray = cv2.cvtColor(sub_img, cv2.COLOR_RGB2GRAY)
+        sub_img_blurr = cv2.GaussianBlur(sub_img_gray, (3, 3), 0)
+        sub_img_edge = cv2.Canny(sub_img_blurr, 50, 150)
+        edge[sub_y1:sub_y2, sub_x1:sub_x2] = sub_img_edge
+
+    h_bar_edge_detect(img, edge_BGR2GRAY)
+
     return edge_BGR2GRAY
 
 
@@ -78,7 +98,7 @@ def character_choice_detect(img: np.ndarray) -> bool:
     h = y2 - y1
 
     img_crop = img[y1:y2, x1:x2]  # 裁剪出目标判断区域
-    img_edge_BGR = canny_edge_detect_BGR(img_crop)
+    img_edge_BGR = canny_edge_detect(img_crop)
     img_rect_detect = rect_detect(img_crop.copy(), img_edge_BGR)
     img_circle_detect = circle_detect(img_crop.copy(), img_edge_BGR)
 
