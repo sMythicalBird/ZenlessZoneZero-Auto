473ec1dc402e09f15c192ab559abf4a8a497b5f8
sMythicalBird <1778133880@qq.com>
2024-09-03 21:55:27 +0800

    update

--- release_prepare.py
+++ release_prepare.py
@@ -4,6 +4,11 @@
 @time:      2024/9/3 14:10
 @author:    sMythicalBird
 """
+import sys
+from pathlib import Path
+
+sys.path.append(str(Path(__file__).parent))  # 将当前目录添加到 sys.path 中
+
 from schema.download import check_file_task
 from gui.api.check_update import release_version
 
